327d333757170bcb47d6587704e34f116f11d728
Alireza Afzal Aghaei <alirezaafzalaghaei@gmail.com>
2024-12-29 14:41:31 +0330

    remove two unnecessary lines

--- runs/NAS-wavKAN.py
+++ runs/NAS-wavKAN.py
@@ -162,7 +162,5 @@ print("ROC-AUC: %.4f" % roc_auc_score(y_test.cpu(), y_pred))
 
 
 results = study.trials_dataframe()
-vars = results[["params_depth", "params_wavelet"]]
-values = results["value"].round(2)
 results.to_csv('results-waveletKAN-%s-%s.csv' % (DATA, datetime.now()))
 
